aafe6e1d5d9ec9875e95e359bef3bfeacd93c131
MD ASH <129009358+md-ash-dot@users.noreply.github.com>
2024-12-14 13:23:39 +0000

    Add event listeners to move tiles and check if tiles moved are adjacent

--- assets/javascript/day7.js
+++ assets/javascript/day7.js
@@ -14,8 +14,63 @@ window.onload = function() {
             tile.id = r.toString() + "-" + c.toString(); // create id for tile with row and column
             tile.src = "/assets/puzzle-images/" + imageOrder.shift() + ".jpg" // run through the image files
 
+            // Moving tiles
+            tile.addEventListener("dragstart", dragStart); // click on tile to drag
+            tile.addEventListener("dragover", dragOver); // drag clicked tiled
+            tile.addEventListener("dragenter", dragEnter); // dragging clicked tile into another tile
+            tile.addEventListener("dragleave", dragLeave); // dragged tile leaving another tile
+            tile.addEventListener("drop", dragDrop); // dragged tile dropped on another tile
+            tile.addEventListener("dragend", dragEnd); // swap two tiles after drag drop
+
             document.getElementById("board").append(tile);
 
         }
+    }
+}
+
+function dragStart() {
+    currentTile = this; // This is the tile being moved
+}
+
+function dragOver(e) {
+    e.preventDefault()
+}
+
+function dragEnter(e) {
+    e.preventDefault()
+}
+
+function dragLeave() {
+
+}
+
+function dragDrop() {
+    targetTile = this; // This is the tile being dropped on
+}
+
+function dragEnd() {
+
+    let currentCoords = currentTile.id.split("-");
+    let r = parseInt(currentCoords[0]); // row of current tile
+    let c = parseInt(currentCoords[1]); // column of current tile
+
+    let targetCoords = targetTile.id.split("-");
+    let r1 = parseInt(targetCoords[0]); // row of target tile
+    let c1 = parseInt(targetCoords[1]); // column of target tile
+
+    let moveRight = r == r1 && c1 == c+1; // adjacent to right 
+    let moveLeft = r == r1 && c1 == c-1; // adjacent to left
+
+    let moveUp = c == c1 && r1 == r-1; // adjacent to above
+    let moveDown = c == c1 && r1 == r+1; // adjacent to below
+
+    let isAdjacent = moveRight || moveLeft || moveUp || moveDown; // check if adjacent
+
+    if (isAdjacent){
+        let currentImage = currentTile.src;
+        let targetImage = targetTile.src;
+
+        currentTile.src = targetImage;
+        targetTile.src = currentImage;
     }
 }
