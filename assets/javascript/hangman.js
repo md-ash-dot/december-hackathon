0760ad8e36eec01dbd089477067122dcb2333d62
shahan15 <k.shahan786@gmail.com>
2024-12-15 15:11:17 +0000

    clicked word exists checker plus display

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -1,16 +1,43 @@
-const keyboardDiv = document.querySelector(".keyboard")
+const hangmanImage = document.querySelector(".hangman-box img");
+const wordDisplay = document.querySelector(".word-display");
+const guessesText = document.querySelector(".guesses-text b");
+const keyboardDiv = document.querySelector(".keyboard");
+
+let currentWord, wrongGuessCount = 0 
+const maxGuesses = 6
 
 const getRandomWord = () => {
     //selects a random word and hint from the word list
     const {word,hint} = wordList[Math.floor(Math.random()*wordList.length)];
-    console.log(word,hint);
+    currentWord = Word
+    console.log(word);
+    document.querySelector(".hint-text").innerText = hint; 
+    wordDisplay.innerHTML = word.split("").map(()=> `<li class="letter"></li>`).join("");
 }
 
-//Creating keyboard dynamicall
+const intGame=(button, clickedLetter) => {
+    //Checking if clickedLetter exists in currentWord
+    if(currentWord.includes(clickedLetter)) {
+        //Showing correct letters on the word display
+        [...currentWord].forEach((letter,index) => {
+            if(letter===clickedLetter) {
+                wordDisplay.querySelectorAll("li")[index].innerText = letter;
+                wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+            }
+        });
+    } else {
+        //if clicked letter doesn't exist then update the wrongGuessedCount and hangman image 
+        wrongGuessCount++;
+        hangmanImage.src = `assets/images/hangman-images/hangman-${wrongGuessCount}.svg`     
+    }
+    guessesText.innerText=`${wrongGuessCount}/${maxGuesses}`;
+}
+//Creating keyboard dynamically
 for (let i = 97; i <= 122;i++) {
     const button =document.createElement("button");
     button.innerText=String.fromCharCode(i)
     keyboardDiv.appendChild(button);
+    button.addEventListener("click", e => intGame (e.target, String.fromCharCode(i)));
 }
 
 getRandomWord()
