24e262bf900ccae10253261938bd1770f266861e
shahan15 <k.shahan786@gmail.com>
2024-12-15 13:12:51 +0000

    selects random word + hint

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -1,8 +1,16 @@
 const keyboardDiv = document.querySelector(".keyboard")
 
+const getRandomWord = () => {
+    //selects a random word and hint from the word list
+    const {word,hint} = wordList[Math.floor(Math.random()*wordList.length)];
+    console.log(word,hint);
+}
+
 //Creating keyboard dynamicall
 for (let i = 97; i <= 122;i++) {
     const button =document.createElement("button");
     button.innerText=String.fromCharCode(i)
     keyboardDiv.appendChild(button);
 }
+
+getRandomWord()
